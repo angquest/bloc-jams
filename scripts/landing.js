d301ced621a9c1e18de5c79920ca2bc9372a616c
angquest <angquest@att.net>
2016-11-08 11:06:01 -0800

    cp-16 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,27 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
+//5
+var animatePoints = function() {
     
-    var revealPoint = function(index) {
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
- 
-    for(var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+    var revealPoint = function() {
+        //7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translate(0)'
+         });
+    };
+ //6
+    $.each($('.point'), reavealPoint);
 };
 
-window.onload = function() {
-    
-    if(window.innerHeight > 950){ //will automatically animate the points(selling-points) on a                                     taller screen where scrolling can't trigger the animation.
-        animatePoint(pointsArray);
+$(window).load = (function() {
+    if($(window).height() > 950){ //will automatically animate the points(selling-points) on a                                     taller screen where scrolling can't trigger the animation.
+        animatePoint();
     }
-    
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    
-    window.addEventListener("scroll", function(event) {
-        console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200; //tells scroll distance for                                                                                      animation
+    $(window).scroll(function(event) {
+        //console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+        if($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
-}        
+});        
     
