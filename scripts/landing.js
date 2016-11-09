ffe1eb07e3531e7212da2d3c2cae1cda8a4b0004
angquest <angquest@att.net>
2016-11-08 19:57:28 -0800

    cp-17 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,21 @@
-//5
 var animatePoints = function() {
     
     var revealPoint = function() {
-        //7
          $(this).css({
              opacity: 1,
              transform: 'scaleX(1) translate(0)'
          });
     };
- //6
-    $.each($('.point'), reavealPoint);
+
+    $.each($('.point'), revealPoint);
 };
 
-$(window).load = (function() {
+$(window).load(function() {
     if($(window).height() > 950){ //will automatically animate the points(selling-points) on a                                     taller screen where scrolling can't trigger the animation.
         animatePoint();
     }
     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200; //tells scroll distance for                                                                                      animation
     $(window).scroll(function(event) {
-        //console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
         if($(window).scrollTop() >= scrollDistance) {
             animatePoints();
         }
