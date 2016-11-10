8e2ba5542a83fedb06ec488a933a761bd696af50
angquest <angquest@att.net>
2016-11-10 11:47:26 -0800

    cp-19 complete

---
+++ scripts/fixtures.js
@@ -0,0 +1,29 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        {title: 'I\'m so Blue', duration: '4:26'},
+        {title: 'Green with Envy', duration: '3:14'},
+        {title: 'Lady in Red', duration: '5:01'},
+        {title: 'Pink Flamingo', duration: '3:21'},
+        {title: 'Magenta, the Pup', duration: '2:15'}
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        {title: 'Hello, Operator?', duration: '1:01'},
+        {title: 'Ring, ring, ring', duration: '5:01'},
+        {title: 'Fits in your pocket', duration: '3:21'},
+        {title: 'Can you hear me now?', duration: '3:14'},
+        {title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
